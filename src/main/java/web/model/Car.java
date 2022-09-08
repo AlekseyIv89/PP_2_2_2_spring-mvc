cc92163945079d1f9ef188b74e34a708c801b767
KOMa <alekseyiv89@mail.ru>
2022-09-08 16:43:53 +0300

    MVC

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,37 @@
+package web.model;
+
+public class Car {
+    private String company;
+    private String model;
+    private int price;
+
+    public Car(String company, String model, int price) {
+        this.company = company;
+        this.model = model;
+        this.price = price;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,8 @@
+package web.service;
+
+public class CarServiceImpl implements CarService {
+    @Override
+    public int getCarsCount() {
+        return 0;
+    }
+}
